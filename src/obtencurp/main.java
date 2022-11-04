b5f245168d173cb220029f19e1c41e9270f5e8d6
Virgilio-LM <virgili0@yahoo.com>
2022-11-03 22:23:28 -0600

    Carga de Proyecto

---
+++ src/obtencurp/main.java
@@ -0,0 +1,23 @@
+package obtencurp;
+
+/**
+ *
+ * @author Virgilio LM <virgili0@yahoo.com>
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+        
+        ventanaUno principal = new ventanaUno();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setTitle("CURP");
+        principal.setVisible(true);
+        
+    }
+    
+}
